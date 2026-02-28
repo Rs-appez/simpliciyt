7c95d0ef687f69b0b6f9150653b4237b4ff491e4
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2026-02-28 07:52:09 +0100

    init commit

---
+++ simpliciyt.js
@@ -0,0 +1,17 @@
+function remove_rich_sections() {
+    const rich_sections = document.getElementsByTagName(
+        "ytd-rich-section-renderer",
+    );
+    rich_sections.forEach((section) => {
+        section.remove();
+    });
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", remove_rich_sections);
+} else {
+    remove_rich_sections();
+}
+
+const observer = new MutationObserver(remove_rich_sections);
+observer.observe(document.body, { childList: true, subtree: true });
